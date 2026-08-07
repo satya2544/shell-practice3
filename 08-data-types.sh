6c541a67f7ad5a55dc47700f7b0ad5805f89c31b
satya2544 <koppolu.satya@gmail.com>
2026-08-07 14:58:48 +0100

    some message

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -8,7 +8,7 @@ SUM=$(($NUMBER1+$NUMBER2))
 echo "SUM is: ${SUM}"
 
 
-LEADERS=("MODI","SATYA","MADHU","NAGARAJU ABVP")
+LEADERS=("MODI" "SATYA" "MADHU" "NAGARAJU ABVP")
 echo "All leaders: ${LEADERS[@]}"
 echo "All leaders: ${LEADERS[0]}"
 echo "All leaders: ${LEADERS[10]}"
