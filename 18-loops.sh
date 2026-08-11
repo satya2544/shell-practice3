d171a424ba27409e1e7dba996bb14492418d5520
satya2544 <koppolu.satya@gmail.com>
2026-08-11 16:06:41 +0100

    some message

--- 18-loops.sh
+++ 18-loops.sh
@@ -34,5 +34,12 @@ fi
 
 for package in $@
 do
-    echo "package is: $package"
+    dnf list installed $package &>>$LOG_FILE
+
+    if [ $? -ne 0 ]; then
+       dnf install $package -y &>>$LOG_FILE
+       VALIDATE $? "$package"
+    else
+        echo -e "$package already installed ... &Y SKIPPING $N"
+    fi
 done
