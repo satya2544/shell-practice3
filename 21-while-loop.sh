a72a53e1e9b6c2c447e9c945198e39c459dde6f9
satya2544 <koppolu.satya@gmail.com>
2026-08-18 21:47:05 +0100

    some message

--- 21-while-loop.sh
+++ 21-while-loop.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
-count=5
+# count=5
 
-echo "Starting countdown.."
+# echo "Starting countdown.."
 
-while [ $count -gt 0 ]
-do
-  echo "Time left: $count"
-  sleep 1 
-  count=$((count - 1))
+# while [ $count -gt 0 ]
+# do
+#   echo "Time left: $count"
+#   sleep 1 
+#   count=$((count - 1))
 
 
-done
+# done
 
-echo "Times up!"
+# echo "Times up!"
+
+
+
+
+
+
+# Loop through the file line by line
+while IFS= read -r line; do
+    # Process each line here
+    echo "Processing line: $line"
+done < 19-script-1.sh
+
+
+
+
+
+
+
+
+
+
