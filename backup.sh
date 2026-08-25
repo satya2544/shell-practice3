3439c760f1b44a48182e4799e028ff183bbdb217
satya2544 <koppolu.satya@gmail.com>
2026-08-25 10:55:15 +0100

    some message

--- backup.sh
+++ backup.sh
@@ -14,7 +14,7 @@ DAYS=${3:-14}
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/backup.log"
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
