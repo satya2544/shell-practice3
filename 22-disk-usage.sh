634f6da8eb4aa35f3f5bb92713f9caaedeac7e4d
satya2544 <koppolu.satya@gmail.com>
2026-08-26 12:15:10 +0100

    some message

--- 22-disk-usage.sh
+++ 22-disk-usage.sh
@@ -2,12 +2,16 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2
+IP_ADDRESS=(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
 
 while IFS= read -r line
 do
   USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
   PARTITION=$(echo $line | awk '{print $7}')
   if [ $USAGE -ge $DISK_THRESHOLD ]; then
-       echo "High Usage on $PARTITION: $USAGE"
+       MESSAGE="High Disk usage on $PARTITION: $USAGE %"
   fi     
 done <<< $DISK_USAGE
+
+echo "Message Body: $MESSAGE"
